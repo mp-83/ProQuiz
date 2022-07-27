dfe1985882ffcdd110ab85636673ea3f58282879
Pittalis Marco <marco.pittalis@prometeon.com>
2022-07-27 22:21:40 +0200

    Removed another file again

--- backend/app/app/domain_service/__init__.py
+++
@@ -1,8 +0,0 @@
-# For a new basic set of CRUD operations you could just do
-
-# from app.domain_entities.user import User
-# from app.syntax_validation.user import User as UserSchema
-#
-# from .base import CRUDBase
-#
-# user = CRUDBase[User, UserSchema, UserSchema](User)
